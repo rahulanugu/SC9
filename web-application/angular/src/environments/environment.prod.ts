f0de63657617852eae03962ea71d1e4dd6496df8
Rahul Reddy Anugu <106054582+rahulanugu@users.noreply.github.com>
2023-09-17 00:31:10 -0400

    Create environment.prod.ts

---
+++ web-application/angular/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  //serverUrl: "https://3.16.14.209",
+  serverUrl: "https://dev.scriptchain.co",
+  //port: 3000,
+  param: "?API_KEY=TiKY7Md2dHpcZo1ih4KbkinTHh7CNTSjseg2ZB3ZiaEC2x1bFA=="
+};
